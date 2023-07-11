b29d3ffcf505f5b1ce49507e7859503a30dbc7a4
TerrificallyTerri <ven22003@byui.edu>
2023-07-11 08:51:07 +0200

    learning practise

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Employee> employees = new List<Employee>();
+
+        employees.Add(new Employee());
+        employees.Add(new HourlyEmployee());
+
+        foreach (Employee employee in employees)
+        {
+            float pay = employee.CalculatePay();
+            Console.WriteLine($"{pay}");
+
+        }
+
+
+
+
+
+    }
+
+
+}
+public class Employee
+{
+    private float salary = 100f;
+    public virtual float CalculatePay()
+    {
+        return salary;
+    }
+}
+public class HourlyEmployee : Employee
+{
+    private float rate = 9f;
+    private float hours = 100f;
+
+    public override float CalculatePay()
+    {
+        return rate * hours;
     }
 }
